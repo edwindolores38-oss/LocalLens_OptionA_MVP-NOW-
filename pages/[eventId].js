a78ffb1a4da6aaef801d28835242dd416bc87321
Edwin dolores <edwindolores38@gmail.com>
2025-12-04 22:54:46 -0500

    Clean up .next folder and fix Layout import

--- pages/[eventId].js
+++ pages/[eventId].js
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import Layout from "../../context/Layout";
+import Layout from "../context/Layout";
+
 
 export default function EventDetail() {
     const router = useRouter();
